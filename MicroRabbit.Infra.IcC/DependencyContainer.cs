689725be3ed3667748e40c29102e7e661b31a918
dpoirier01 <poirier9155@comcast.net>
2019-11-17 13:17:53 -0800

    Partially completed Banking Microservice

--- MicroRabbit.Infra.IcC/DependencyContainer.cs
+++ MicroRabbit.Infra.IcC/DependencyContainer.cs
@@ -1,9 +1,12 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MicroRabbit.Infra.IcC
 {
@@ -11,7 +14,17 @@ namespace MicroRabbit.Infra.IcC
 	{
 		public static void RegisterServices(IServiceCollection services)
 		{
-			services.AddTransient<IEventBus, RabbitMQBus>();
+			//services.AddTransient<IEventBus, RabbitMQBus>();
+			services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+			{
+				var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+				return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
+			});
+
+			services.AddTransient<IAccountService, AccountService>();
+
+			services.AddTransient<IAccountRepository, AccountRepository>();
+			services.AddTransient<BankingDbContext>();
 		}
 	}
 }
